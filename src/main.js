b429cec11c62aad677772790d22695d83995ed0d
Dennis Koluris <dkoluris@outlook.com>
2018-01-12 20:51:29 +0200

    Added bins.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,33 @@
 // 0x200 - 0xffff start/end
 
 var chip8 = (function() {
+  // General purpose
+  var v = [], pc, i, timer;
+
+  function read(pc) {
+    exit('Mem read -> '+pc);
+  }
+
+  function exit(str) {
+    throw str;
+  }
+
   return {
     reset: function() {
+      for (var i=0; i<16; i++) {
+        v[i] = 0;
+      }
+
+      pc = 0x200;
+      i  = 0;
+
+      // Timers
+      timer = {
+        root: 0, sound: 0
+      };
+
+      // Run
+      read(pc);
     }
   }
 })();
